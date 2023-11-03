2f85c25dbcbf950e191606693767eb448c1014f7
AliaksandraPapko <saschkamihnovez66@gmail.com>
2023-11-03 09:29:18 +0300

    сделала наконец-то свой первый генератор-пресдказаний=)

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 /* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
+const forecastContainer = document.querySelector('.current-forecast');
+const predictForecast = document.querySelector('.forecast-btn');
+const oldForecasts = document.querySelector('.forecasts');
 
+predictForecast.addEventListener('click', function() {
+    const textForecast = [
+        "Вы не узнаете, если не попытаетесь",
+        "Всему свое время",
+        "Никогда не забывайте о себе",
+        "Не вмешивайся в чужие дела"
+    ];
+    const randomForecast = textForecast[Math.floor(Math.random() * textForecast.length)];
+    const forecast = document.querySelector('.current-forecast h1');
+    forecast.textContent = `${randomForecast}`;
+
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(0);
+        max = Math.floor(100);
+        return randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    getRandomIntInclusive();
+
+    const odds = document.querySelector('.current-forecast p');
+    odds.textContent = `Вероятность - ${randomValue}%`;
+
+    function createNewForecast(forecast, odds) {
+        const template = document.querySelector('#forecast-item');
+        const newForecast = template.content.cloneNode(true);
+
+        newForecast.querySelector('h3').textContent = forecast;
+
+        newForecast.querySelector('p').textContent = odds;
+
+        oldForecasts.append(newForecast);
+    }
+    createNewForecast(forecast.textContent, odds.textContent);
+});
 /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
 
 /* Подставляй текст нового предсказания в .current-forecast h1 */
